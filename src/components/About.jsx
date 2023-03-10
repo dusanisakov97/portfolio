cc77dc6879e581a5b999cc0cf1cb426ca8688370
dusanisakov97 <dusisakov@gmail.com>
2023-03-10 05:08:13 +0100

    fix

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,7 +5,7 @@ const About = () => {
     <div name="about" className="w-full h-[50vh] bg-[white] text-[#1b3fab]">
       <div className="flex flex-col justify-center items-center w-full h-full">
         <div className="max-w-[1000px] w-full grid gap-8">
-          <div className="sm:text-center pb-8 pl-4">
+          <div className="text-center pb-8 pl-4">
             <p className="text-4xl text-left font-bold inline border-b-4 border-[#1b3fab] m-4">
               About
             </p>
